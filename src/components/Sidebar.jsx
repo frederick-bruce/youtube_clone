236734ab5f33fbb73a39306264619df0b3d5c3e3
Frederick Bruce <frederick.bruce.dev@gmail.com>
2022-11-20 06:51:45 -0500

    Logic
    video logic from RapidAPi (Youtube V3)

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,9 +2,7 @@ import { Stack } from "@mui/material";
 
 import { categories } from "../utils/constants";
 
-const selectedCategory = "New";
-
-const Sidebar = () => (
+const Sidebar = ({ selectedCategory, setSelectedCategory }) => (
   <Stack
     direction="row"
     sx={{
@@ -17,6 +15,7 @@ const Sidebar = () => (
       <button
         type="button"
         className="category-btn"
+        onClick={() => setSelectedCategory(category.name)}
         style={{
           background: category.name
             === selectedCategory && "#FC1503",
