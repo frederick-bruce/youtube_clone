d42e288b34e7ab57d73ac422ceb0e535227fb601
Frederick Bruce <frederick.bruce.dev@gmail.com>
2022-11-21 18:27:24 -0500

    styling fix
    fixed video card styles

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -6,27 +6,24 @@ import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../utils/constants";
 
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => (
-  <Card sx={{ width: { md: "326px", xs: "356px", xl: "390px" }, boxShadow: "none", borderRadius: 0 }}>
-    <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+  <Card sx={{ width: { xs: "300px", sm: "358px", md: "320px" }, boxShadow: "none", borderRadius: 0, justifyContent: "center", alignItems: "center" }}>
+    <Link to={videoId ? `/video/${videoId}` : "/video/cV2gBU6hKfY"}>
       <CardMedia
         image={snippet?.thumbnails?.high?.url || demoThumbnailUrl}
         alt={snippet?.title}
-        sx={{ width: { md: "326px", xs: "356px", xl: "390px", justifyContent: "center", height: 180 } }}
+        sx={{ width: { xs: "300px", sm: "358px" }, height: 180, display: "flex", alignItems: "center" }}
       />
     </Link>
-    <CardContent
-      sx={{ backgroundColor: "#1e1e1e", height: "106px" }}
-    >
+    <CardContent sx={{ backgroundColor: "#1E1E1E", height: "106px" }}>
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-        <Typography variant="subtitle1" fontWeight="bold" color="#fff">
-          {snippet?.title.slice(0, 60)
-                || demoVideoTitle.slice(0, 60)}
+        <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+          {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
         </Typography>
       </Link>
       <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
-        <Typography variant="subtitle2" fontWeight="bold" color="gray">
+        <Typography variant="subtitle2" color="gray">
           {snippet?.channelTitle || demoChannelTitle}
-          <CheckCircleIcon sx={{ fontSize: 12, color: "gray", ml: "5px" }} />
+          <CheckCircleIcon sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
         </Typography>
       </Link>
     </CardContent>
